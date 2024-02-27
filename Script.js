c3678786e4f0f058ee1d73f13f5e8eb795877c09
Ingrid <ingrid-rose.moukala@outlook.fr>
2024-02-27 14:18:30 +0100

    modif

---
+++ Script.js
@@ -0,0 +1,6 @@
+function clickmenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".corps-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
